ab89e35f0163f87e1d5d8d29267c771f67bd6438
Artash Margaryan <artash@artash-pc>
2020-12-22 17:46:53 +0400

    03

--- src/page1.js
+++ src/page1.js
@@ -6,8 +6,22 @@ export class Page extends PIXI.Container {
       height: config.height,
       backgroundColor: config.color,
     });
+    this.scaleVal = {
+      title: [[0.8, 0.8]],
+    };
     this.config = config;
+    this.pageControl(config);
     this.build();
+  }
+
+  pageControl(config) {
+    this.graph = new PIXI.Graphics();
+
+    this.scaleVal = {
+      title: [],
+      center: [],
+      futer: [],
+    };
   }
 
   build() {
@@ -23,14 +37,44 @@ export class Page extends PIXI.Container {
     this.titleContenier = new PIXI.Container();
     const title = new PIXI.Sprite.from('title');
     title.anchor.set(0.5);
-    this.titleContenier.addChild(title);
+    this.titleContenier.addChild((this.title = title));
     this.addChild(this.titleContenier);
   }
   toCorrectTitle() {
     const { width, height } = this.config;
-    this.titleContenier.position.set(width / 2, height / 7);
+    this.titleContenier.position.set(width / 2, Math.max(height / 7, 50));
     this.titleContenier.children.width = 50;
-    this.titleContenier.children.height = 50;
     console.warn(this.titleContenier.children);
+
+    this.title.scale.set(0.8, 0.8);
+  }
+
+  buildCenterContainer() {
+    this.centerContenier = new PIXI.Container();
+    const contLeft = new PIXI.Container();
+    const contRight = new PIXI.Container();
+    graphics.beginFill(0xde3249);
+    graphics.drawRect(50, 50, 100, 100);
+    graphics.endFill();
+    t;
+  }
+
+  toCorrectCenter() {}
+
+  buildFuterContainer() {
+    this.titleContenier = new PIXI.Container();
+    const title = new PIXI.Sprite.from('title');
+    title.anchor.set(0.5);
+    this.titleContenier.addChild((this.title = title));
+    this.addChild(this.titleContenier);
+  }
+
+  toCorrectFuter() {
+    const { width, height } = this.config;
+    this.titleContenier.position.set(width / 2, Math.max(height / 7, 50));
+    this.titleContenier.children.width = 50;
+    console.warn(this.titleContenier.children);
+
+    this.title.scale.set(0.8, 0.8);
   }
 }
