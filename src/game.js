ab47571fb07f7ff5a5f05b0fe2a67e72056a1aea
Artash Margaryan <artash@artash-pc>
2020-12-24 18:00:22 +0400

    07

--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import * as PIXI from 'pixi.js';
 import { Page } from './page1';
 import { gsap, Bounce } from 'gsap';
+import { PixiPlugin } from 'gsap/PixiPlugin';
 
 export class Game extends PIXI.Application {
   constructor() {
@@ -9,6 +10,9 @@ export class Game extends PIXI.Application {
       height: window.innerHeight,
       backgroundColor: 0xffffff,
     });
+
+    gsap.registerPlugin(PixiPlugin);
+    PixiPlugin.registerPIXI(PIXI);
 
     document.body.appendChild(this.view);
     window.addEventListener('resize', this._rebuildStage.bind(this));
@@ -60,7 +64,7 @@ export class Game extends PIXI.Application {
 
   buildHand() {
     const hand = new PIXI.Sprite.from('hand');
-    hand.position.set(this.boxPos[0].x, this.boxPos[0].y + 1000);
+    hand.position.set(this.boxPos[0].x, this.boxPos[0].y);
     this.stage.addChild((this.hand = hand));
     // let gsapLine1 = gsap.to(hand, { direction: 4, x: this.boxPos[0].x, y: this.boxPos[0].y });
     this.animation(hand);
@@ -68,13 +72,17 @@ export class Game extends PIXI.Application {
     // tl.reverse();
   }
   animation(hand) {
-    const tl = gsap.timeline({ repeat: 2, repeatDelay: 1 });
+    const tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
     tl.to(hand, { x: this.boxPos[0].x, y: this.boxPos[0].y, duration: 2 });
-    tl.to(hand, { x: this.boxPos[1].x, y: this.boxPos[1].y + 1, duration: 1 });
+    tl.to(hand, { pixi: { scaleX: 0.5, scaleY: 0.5 }, duration: 3, yoyo: true });
+    tl.to(hand, { x: this.boxPos[1].x, y: this.boxPos[1].y + 1, duration: 2 });
+    tl.to(hand, { pixi: { scaleX: 0.5, scaleY: 0.5 }, duration: 1 });
     tl.to(hand, { opacity: 0, duration: 1 });
 
-    // then we can control the whole thing easily...
+    // tl.resume();
 
-    tl.resume();
+    // setTimeout(() => {
+    //   gsap.killTweensOf(hand);
+    // }, 3000);
   }
 }
